647052e6edb81f5147a505293953ab6b91fa3143
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-10-09 22:13:45 +0100

    push, pall

--- 0-push_pall.c
+++ 0-push_pall.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 
 /**
- * push - function that puts a number into the top node
+ * push - puts a number into the top node
  * @head: a pointer to the stack
- * @line_number: lines number
+ * @line_number: the number of lines
  *
  */
 void push(stack_t **head, unsigned int line_number)
@@ -12,13 +12,16 @@ void push(stack_t **head, unsigned int line_number)
 
 	(void)line_number;
 	new_node = malloc(sizeof(stack_t));
+	global.newnode = new_node;
 	if (new_node == NULL)
 	{
 		free(new_node);
 		fprintf(stderr, "Error: malloc failed\n");
+		free(global.line);
+		fclose(global.fp);
 		exit(EXIT_FAILURE);
 	}
-	new_node->n = global_number;
+	new_node->n = global.global_number;
 	if (*head != NULL)
 		(*head)->prev = new_node;
 	new_node->prev = NULL;
@@ -27,43 +30,68 @@ void push(stack_t **head, unsigned int line_number)
 }
 
 /**
- * pall - function that prints all elements of the stack
+ * pall - prints all elements of the stack
  * @stack: a pointer to the stack
- * @line_number: lines number
+ * @line_number: the number of lines
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	int i;
+	int i = 0;
 	stack_t *temp = *stack;
 
 	(void)line_number;
-	for (i = 0; temp != NULL; i++)
+	for (; temp != NULL; temp = temp->next)
 	{
-		temp = temp->next;
 		printf("%d\n", temp->n);
+		i++;
 	}
 }
 
 /**
- * check_digit - Function that checks if the data is an integer
+ * check_digit_push - checks if data is an integer
  * @tokens: a token
+ * @line_number: the number of lines
  * Return: 0 if true, 1 if was converted
  */
-
-int check_digit(char *tokens)
+int check_digit_push(char *tokens, unsigned int line_number)
 {
 	int i = 0;
 
 	if (tokens == NULL)
-		return (0);
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		free(global.line);
+		fclose(global.fp);
+		free_stack_t(global.newnode);
+		exit(EXIT_FAILURE);
+	}
 	if (tokens[i] == '-')
 		i++;
-	for (; tokens[i]; i++)
+	for (; tokens[i] != '\0'; i++)
 	{
-		if (tokens[i] < 48 || tokens[i] > 57)
-			return (0);
+		if (tokens[i] < '0' || tokens[i] > '9')
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line_number);
+			free(global.line);
+			fclose(global.fp);
+			free_stack_t(global.newnode);
+			exit(EXIT_FAILURE);
+		}
 	}
 
-	global_number = atoi(tokens);
+	global.global_number = atoi(tokens);
 	return (1);
+}
+
+/**
+ * check_digit - checks if data is an integer
+ * @tokens: a token
+ * @line_number: the number of lines
+ * Return: 0 if true, 1 if was converted
+ */
+int check_digit(char *tokens, unsigned int line_number)
+{
+	(void)line_number;
+	(void)tokens;
+	return (0);
 }
