b3b2057121860f43526e347e7e66d4401c1cba7d
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-10-09 21:22:36 +0100

    nop

---
+++ 5-nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - fuction that does nothing
+ * @head: a pointer to the stack
+ * @line_number: lines number
+ *
+ */
+void nop(stack_t **head, unsigned int line_number)
+{
+	(void)head;
+	(void)line_number;
+}
