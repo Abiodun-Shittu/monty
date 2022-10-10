d2d8c1e909bac84328b321b0d8f2d53e1c01e763
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-10-10 01:07:00 +0100

    add

--- 4-add.c
+++ 4-add.c
@@ -1,15 +1,21 @@
 #include "monty.h"
 
 /**
- * add - a function that adds the top numbers of the stack
+ * add - a function that adds the top numbers
  * @stack: a pointer to the stack
- * @line_number: lines number
+ * @line_number: the number of lines
  */
 void add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp, *aux, *node;
 
-	(void)line_number;
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		free(global.line);
+		fclose(global.fp);
+		exit(EXIT_FAILURE);
+	}
 	if ((*stack)->next != NULL)
 	{
 		(*stack)->next->n += (*stack)->n;
